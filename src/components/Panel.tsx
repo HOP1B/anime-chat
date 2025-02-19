b9a688c3421f6eb948f42759d19e7f05f9ebd40c
Khegal <khegal95@gmail.com>
2025-02-19 17:11:39 +0800

    feat(added panel to layout): added panel to layout

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -7,11 +7,13 @@ const Panel = () => {
   return (
     <>
       <div className="flex min-h-screen bg-gray-100 ">
-        <Card className="w-96 shadow-lg">
+        <Card className="w-64 shadow-lg">
           <CardHeader>
-            <CardTitle>Welcome</CardTitle>
+            <CardTitle>Anime-chat.Ai</CardTitle>
           </CardHeader>
-          <CardContent>
+          <CardContent className="flex gap-2 flex-col">
+            <Button className="w-[70px] rounded-3xl">Create</Button>
+            <Button>Discover</Button>
             <p className="text-gray-700">
               This is a simple Next.js 15.1 component using Tailwind CSS and
               ShadCN UI.
