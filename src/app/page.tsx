f9f2780ecfb1535e1cbd54c5a48414d947690e41
24HP0498 24HP0498 <24HP0498@G305-CC0244.local>
2025-02-26 18:13:24 +0800

    feat(home): home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="h-screen mx-96 pt-10">
+        <div className="flex text-gray-500 justify-between">
+          <div>
+            <p>Welcome back,</p>
+            <p>user</p>
+          </div>
+          <div className="relative">
+            <Input
+              className="w-80 h-12 py-4 pl-10  bg-slate-100 rounded-full"
+              placeholder="Search for characters"
+            />
+            <Search className="absolute bottom-3 left-3 w-5" />
+          </div>
+        </div>
+        <div className="mt-20">
+          <p>For you</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
